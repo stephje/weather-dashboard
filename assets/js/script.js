19bdf7fbf22233a582087ee03f4452a92ad2df00
stephje <s.jenkins3018@gmail.com>
2021-04-15 14:24:26 +0000

    Restructured promises

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,24 +2,14 @@ const openWeatherKey = "d8b9c9f29acf75b9bc89cdc565bacf19"
 
 //event listener for the location search button
 var searchButton = document.querySelector("#search-button");
-searchButton.addEventListener("click", getWeatherData);
+searchButton.addEventListener("click", displayWeatherReport);
 
-function getWeatherData(event) {
-    //prevent default behaviour of form submission
-    event.preventDefault();
-    
-    //function getCityName retrieves the user input and sanitises it
-    cityName = getCityName();
-    
+function getCityLocation(cityName) {
     //this URL will be used to get the latitude and longitude of the relevant location
     var locationQueryURL = ("https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + openWeatherKey)
-
-    // fetch(queryURL)
-    // .then(response => response.json())
-    // .then (data => console.log(data))
     
     //fetch the data
-    fetch(locationQueryURL)
+    return fetch(locationQueryURL)
     .then(
         function getResponse(response){
             if (response.status === 404) {
@@ -27,28 +17,35 @@ function getWeatherData(event) {
             } else {
                 return response.json();
             } 
-        })
-    .then(
+        });
+}
+
+function getWeatherForecast(lat,lon) {
+    //this URL will be used to get the weather forecast data for the relevant location
+    var forecastQueryURL = ("https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=minutely,hourly,alerts&appid=" + openWeatherKey);
+
+    return fetch(forecastQueryURL)
+    .then(response => response.json())
+}
+
+function displayWeatherReport(event) {
+    //prevent default behaviour of form submission
+    event.preventDefault();
+    
+    //function getCityName retrieves the user input and sanitises it
+    var cityName = getCityName();
+
+    getCityLocation(cityName).then(
         function getLocationData(locationData){
-            //TEST DELETE LATER
-            console.log(locationData);
+
             var lat = locationData.coord.lat;
             var lon = locationData.coord.lon;
             
-            //TEST DELETE LATER
-            console.log(lat, lon)
-            
-            //this URL will be used to get the weather forecast data for the relevant location
-            var forecastQueryURL = ("https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=minutely,hourly,alerts&appid=" + openWeatherKey);
-
-            fetch(forecastQueryURL)
-            .then(response => response.json())
-            .then(forecastData =>  console.log(forecastData))
-
-            //DO SOMETHING WITH THE OUTPUT HERE
+            getWeatherForecast(lat, lon).then(weatherForecast => {
+                console.log("This is the weather forecast:", weatherForecast)
+            })
 
         })
-
 }
 
 //get user input from city search
